f380b3ed73cd0a65aedfd199c72d26fda57c27a9
abdo <abdoabdo2810575@gmail.com>
2024-05-04 07:23:38 +0300

    first merg shell and parser

---
+++ SQL_DBMS/src/main/java/com/mycompany/sql_dbms/SQL_DBMS.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sql_dbms;
+
+/**
+ *
+ * @author hp
+ */
+public class SQL_DBMS {
+
+    public static void main(String[] args) {
+       Shell x = new Shell();
+    
+    }
+}
